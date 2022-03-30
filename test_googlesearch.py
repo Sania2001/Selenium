0f51c1189e2025a49da3694d055fc7ba64230952
Sania2001 <saniagshinde@gmail.com>
2022-03-30 14:54:50 +0530

    Google.com

--- test_googlesearch.py
+++ test_googlesearch.py
@@ -1,11 +1,10 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-
+import time
 driver = webdriver.Chrome(ChromeDriverManager().install())
 print("Test case started")
 driver.maximize_window()
 driver.get("https://www.google.com/")
-import time
 time.sleep(5)
 driver.close()
 print("Test case has successfully completed")
